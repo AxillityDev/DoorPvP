f87e43b72d3f1d0195994d9b0784eba5bda3377e
Axillity <ttvtorian33@gmail.com>
2025-04-05 21:34:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,6 @@
         document.addEventListener('DOMContentLoaded', function() {
             // Tier data
-            const tiers = [
-                {
-                    tier: "Tier 1",
-                    color: "var(--tier1-color)",
-                    players: []
-                },
-                {
-                    tier: "Tier 2",
-                    color: "var(--tier2-color)",
-                    players: []
-                },
-                {
-                    tier: "Tier 3",
-                    color: "var(--tier3-color)",
-                    players: [
-                        { name: "Axillity", isHT: true },
-                        { name: "Bokshaq", isHT: false },
-                        { name: "WojPRO1", isHT: false }
-                    ]
-                },
-                {
-                    tier: "Tier 4",
-                    color: "var(--tier4-color)",
-                    players: []
-                },
-                {
-                    tier: "Tier 5",
-                    color: "var(--tier5-color)",
-                    players: []
-                }
-            ];
+            import tiers from './TierLista.js';
             
             const tierContainer = document.getElementById('tier-container');
             
