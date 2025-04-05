c0416c62536c78f1e5327b3592ad376145cb09f5
Axillity <ttvtorian33@gmail.com>
2025-04-05 21:39:59 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,58 @@
         document.addEventListener('DOMContentLoaded', function() {
-            // Tier data
-            import tiers from './TierLista.js';
+            // Zmień Tiery
+           const tiers = [
+                {
+                    tier: "Tier 1",
+                    color: "var(--tier1-color)",
+                    players: []
+                },
+                {
+                    tier: "Tier 2",
+                    color: "var(--tier2-color)",
+                    players: []
+                },
+                {
+                    tier: "Tier 3",
+                    color: "var(--tier3-color)",
+                    players: [
+                        { name: "Bokshaq", isHT: false },
+                        { name: "WojPRO1", isHT: false }
+                    ]
+                },
+                {
+                    tier: "Tier 4",
+                    color: "var(--tier4-color)",
+                    players: [
+                        { name: "Obiberek", isHT: false }
+]
+                },
+                {
+                    tier: "Tier 5",
+                    color: "var(--tier5-color)",
+                    players: [
+                        { name: "Axillity", isHT: false }
+                    ]
+                }
+            ];
+//  NIE TYKAĆ
+
+
+
+
+
+
+
+
+
+
+                
+
+
+
+
+
+
+
             
             const tierContainer = document.getElementById('tier-container');
             
