93e23fa0237d6623b9afb001fa842e806b3df3fa
Axillity <ttvtorian33@gmail.com>
2025-04-05 09:16:13 +0200

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        customGray: {
+          DEFAULT: '#1A1A2E',
+          lighter: '#2E2E4F',
+        },
+      },
+    },
+  },
+  plugins: [],
+};
